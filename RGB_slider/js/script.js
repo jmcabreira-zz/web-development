f3d7d6273d1c1d987cb871f67a7e9224c0c06496
jmcabreira <jm_cabreira@id.uff.br>
2020-06-11 00:06:45 +0100

    refactor: Add rgbSlider function

--- RGB_slider/js/script.js
+++ RGB_slider/js/script.js
@@ -1,13 +1,15 @@
 var input = document.querySelectorAll('input');
 console.log(input);
 
-for (var i = 0; i < input.length; i++) {
-  input[i].addEventListener('input', function () {
-    var red = document.getElementById('red').value,
-      green = document.getElementById('green').value,
-      blue = document.getElementById('blue').value;
+function rgbSlider() {
+  var red = document.getElementById('red').value,
+    green = document.getElementById('green').value,
+    blue = document.getElementById('blue').value;
 
-    var display = document.getElementById('display');
-    display.style.background = 'rgb(' + red + ',' + green + ',' + blue + ')';
-  });
+  var display = document.getElementById('display');
+  display.style.background = 'rgb(' + red + ',' + green + ',' + blue + ')';
+}
+
+for (var i = 0; i < input.length; i++) {
+  input[i].addEventListener('input', rgbSlider);
 }
